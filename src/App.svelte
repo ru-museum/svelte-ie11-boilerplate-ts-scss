0058ece4ed9160436337d6a328a9b278ae272144
ru-museum <museum_ru@yahoo.co.jp>
2026-04-15 19:56:14 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -26,11 +26,61 @@
 </main>
 
 <div class="container">
+
+<h2>環境構築</h2>
+
+  <h3>● package.json</h3>
+<pre>
+"@babel/core": "^7.29.0",
+"@babel/plugin-syntax-dynamic-import": "7.8.3",
+"@babel/plugin-transform-runtime": "^7.29.0",
+"@babel/preset-env": "^7.29.2",
+"@babel/runtime": "^7.29.2",
+"@el3um4s/rollup-plugin-css-only": "^1.0.0",
+"@el3um4s/rollup-plugin-terser": "^1.0.2",
+"@rollup/plugin-babel": "^7.0.0",
+"@rollup/plugin-commonjs": "^29.0.2",
+"@rollup/plugin-node-resolve": "^16.0.3",
+"@rollup/plugin-replace": "^6.0.3",
+"@rollup/plugin-typescript": "^12.3.0",
+"@tsconfig/svelte": "^5.0.8",
+"core-js": "^3.49.0",
+"marked": "^18.0.0",
+"npm-run-all": "^4.1.5",
+"rollup": "^4.60.1",
+"rollup-plugin-livereload": "^2.0.5",
+"rollup-plugin-svelte": "^7.2.3",
+"sass": "^1.99.0",
+<strong>"svelte": "^4.2.19"</strong>, (latest: 5.55.4)
+"svelte-check": "^4.4.6",
+"svelte-preprocess": "^6.0.3",
+"tslib": "^2.8.1",
+<strong>"typescript": "^5.9.3"</strong>, (latest: 6.0.2)
+"whatwg-fetch": "^3.6.20"
+</pre>
+
+
+  <h3>● TypeScript</h3>
+<pre>
+// Warning：Node 24系以後から表示される様です。
+[DEP0190] DeprecationWarning: Passing args to a child process with shell<br> option true can lead to security vulnerabilities, as the arguments are<br> not escaped, only concatenated.
+(Use `node --trace-deprecation ...`to show where the warning was created)
+
+１）指示のオプション指定では別の警告が表示されます。
+    package.json
+    "scripts": &#123;
+     "dev": "<strong>NODE_OPTIONS=--trace-deprecation</strong> rollup -c -w",
+
+２）全てを非表示にするには <strong>--no-deprecation</strong> 或いは、23系を使用します。
+   // build その他も同様です
+   - "dev": "rollup -c -w",
+   + "dev": "NODE_OPTIONS='--no-deprecation' rollup -c -w",
+</pre>
+
 <h2>動作テスト</h2>
   <h3>● TypeScript</h3>
 
 <pre>
-   <div>
 &lt;script <strong>lang="ts"</strong>&gt;
 
    const buttons<strong>: string</strong> = [
@@ -40,10 +90,7 @@
    ];
 
 &lt;/script&gt;
-
-   </div>
 </pre>    
-
 
      <div style="text-align:center;">
          {#each buttons as {name, color}, i}
@@ -56,15 +103,12 @@
          {/each}
      </div>
 
-
-
   <h3>● SCSS</h3>
   
-<pre>    
-   <div>
+<pre>
 &lt;style <strong>lang="scss"</strong>&gt;
 
-   //変数の宣言 
+   /* 変数の宣言 */ 
    <strong>$bgcolor</strong>: #e1f5fe;
    <strong>$width</strong>:400px;
    <strong>$color</strong>: green;
@@ -78,8 +122,6 @@
    &#125;
    
 &lt;/style&gt;
-   </div>
-   
 </pre>    
 
    <div class="sample">
@@ -104,7 +146,6 @@
 		width: 600px;
 		margin: 0 auto;
 	}
-
 	
 	h1 {
 		color: #ff3e00;
@@ -113,13 +154,12 @@
 		font-weight: 100;
 	}
 
-
-   //変数の宣言 
+   /* 変数の宣言 */
    $bgcolor: #e1f5fe;  
    $width: 400px;
    $color: green;
    
-   pre div {
+   pre {
       background-color: #f4f9ff;
       padding: 12px;
    }
@@ -136,6 +176,8 @@
       }
       .content p {
          color: $color;
+         font-size:2em;
+         text-align:center;
       }
    }
 
@@ -144,12 +186,12 @@
    }
 
    .btn{
+      font-size:3em;
       margin-right: 20px;
       color:#fff;
-      padding:16px;
+      padding:0.6em;
       border-radius:12px;
    }
-
 
 	@media (min-width: 640px) {
 		main {
